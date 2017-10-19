866bbad248359faf4f8ecb22e370a64219efc415
Vincent FUNG <vincent201399@gmail.com>
2017-10-19 18:56:31 +0800

    Integration JIRA rest API. part. will return pid and issuetype not set yet. Will ask atlassian for help.

---
+++ http_websocket/runtimelog/post2jira.py
@@ -0,0 +1,66 @@
+# Author = 'Vincent FUNG'
+# Create = '2017/10/19'
+#                       ::
+#                      :;J7, :,                        ::;7:
+#                      ,ivYi, ,                       ;LLLFS:
+#                      :iv7Yi                       :7ri;j5PL
+#                     ,:ivYLvr                    ,ivrrirrY2X,
+#                     :;r;j5P.7r:                :ivuksxerfnli.
+#                    :iL7::,:::iiirii:ii;::::,,irvF7rvvLujL7ur
+#                   ri::,:,::i:iiiiiii:i:irrv177JX7rYXqZEkvv17
+#                ;i:, , ::::iirrririi:i:::iiir2XXvii;L8OGJr71i
+#              :,, ,,:   ,::irii:i:::.irii:i:::j1jri7ZBOS7ivv,
+#                 ,::,    ::rv77iiiriii:iii:i::,rvLrYXqZEk.Li
+#             ,,      ,, ,:ir7ir::,:::i;ir:::i:i::rSGGYri712:
+#           :::  ,v7r:: ::rrv77:, ,, ,:i7rrii:::::, ir7ri7Lri
+#          ,     2OBBOi,iiir;r::        ,irriiii::,, ,iv7Luur:
+#        ,,     i78MBBi,:,:::,:,  :7FSL: ,iriii:::i::,,:rLqXv::
+#        :      iuMMP: :,:::,:ii;Y#LYBB0viiii:i:iii:i:::iJqL;::
+#       ,     ::::i   ,,,,, ::LuBBu BBBBBErii:i:i:i:i:i:i:r77ii
+#      ,       :       , ,,:::rruBZ1MBBqi, :,,,:::,::::::iiriri:
+#     ,               ,,,,::::i:  :i:i:i:i.       ,:,, ,:::ii;i7:
+#    :,       rjujLYLi   ,,:::::,:::::::::,,   ,:i,:,,,,,::i:iii
+#    ::      BBBBBBBBB0,    ,,::: , ,:::::: ,      ,,,, ,,:::::::
+#    i,  ,  ,8@VINCENTBBi     ,,:,,     ,,, , ,   , , , :,::ii::i::
+#    :      iZMOMOMBBM2::::::::::,,,,     ,,,,,,:,,,::::i:irr:i:::,
+#    i   ,,:;u0MBMOG1L:::i::::::  ,,,::,   ,,, ::::::i:i:iirii:i:i:
+#    :    ,iuUuuXUkFu7i:iii:i:::, :,:,: ::::::::i:i:::::iirr7iiri::
+#    :     :rkwwiBivf.i:::::, ,:ii:::::::i:::::i::,::::iirrriiiri::,
+#     :      5BMBBBBBBSr:,::rv2kuii:::iii::,:i:,, , ,,:,:ia5wf88s5.,
+#          , :r50EZ8MILOVEYOUBZP7::::i::,:::::,: :,:,::i;rrririiii::
+#              :jujYY7LS0ujJL7r::,::i::,::::::::::::::iirirrrrrrr:ii:
+#           ,:  :::,: :,,:,,,::::i:i:::::::::::,,:::::iir;ii;7v7;ii;i,
+#           ,,,     ,,:,::::::i:iiiii:i::::,, ::::iiiii;L8OGJrf.r;7:i,
+#        , , ,,,:,,::::::::iiiiiiiiii:,:,:::::::::iiir;ri7vL77rrirri::
+#         :,, , ::::::::i:::i:::i:i::,,,,,:,::i:i:::iir;:::i:::i:ii:::
+#                          _
+#                      o _|_           __
+#                      |  |      (__| (__) (__(_
+#                                   |
+
+from jira import JIRA
+import urllib
+
+jira = JIRA('http://192.168.1.118:8080/secure/QuickCreateIssue.jspa?decorator=none', basic_auth=('zhaoyefeng', 'lancelot0318'))
+
+summary = 'auto add id 1'
+desc = 'auto add \n id 1'
+env = 'env \n id 1'
+
+issue_dict = {
+    'pid': '10100',
+    'issuetype': '1',
+    'customfield_10004': 10068,
+    'customfield_10003': 10029,
+    'summary': summary,
+    'versions': 10601,
+    'customfield_10002': 10014,
+    'customfield_10002:1': 10018,
+    'priority': 3,
+    'assignee': 'zhaoyefeng',
+    'environment': env,
+    'description': desc,
+    'reporter': 'zhaoyefeng'
+}
+
+new_issue = jira.create_issue(fields=issue_dict)
